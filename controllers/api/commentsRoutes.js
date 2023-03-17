2d1f217d770a6896f911887e3f63758ef0afa2a3
stev232 <stev232@yahoo.com>
2023-03-16 20:31:55 -0500

    updated the routes (controllers) for the project to allow for better reading of the database.

---
+++ controllers/api/commentsRoutes.js
@@ -0,0 +1,25 @@
+const router = require('express').Router();
+const { Comments } = require('../../models');
+
+router.get('/:id', async (req, res) => {
+  try {
+    const commentsData = await Comments.findOne({ where: { blog_post_id: req.params.id } });
+    if (!commentsData) {
+      res.status(400).json({ message: 'there are no comments for this post' });
+      return;
+    }
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const commentsData = await Comments.create(req.body);
+    res.status(200).json(commentsData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
